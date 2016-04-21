c1d458a5bb66e9b618f8a06b07bea647c69b9e3f
Uili-Simteau <simteau@gmail.com>
2016-04-22 11:03:39 +1200

    new indexpage and blog template

---
+++ scriptTLW.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+$(window).scroll(function(argument) {
+
+var wScroll = $(this).scrollTop();
+//console.log(wScroll);
+
+$('.title').css({
+'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+})
+
+
+$('.foreTrooper').css({
+'transform' : 'translate(0px, '+ wScroll /30 +'%)'
+});
+
+
+  $('.backTrooper').css({
+'transform' : 'translate(0px, -'+ wScroll /40 +'%)'
+});
+
+
+if (wScroll > $('.largeWindow').offset().top - $(window).height()) {
+  console.log("hell no");
+  $('.largeWindow').css({'background-position':'center '+ (wScroll - $('.largeWindow').offset().top) +'px'});
+
+}
+});
+
+$("button").click("animated hinge");
+
+});
